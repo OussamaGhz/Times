55abb6a999677f34b28c87398f6d77838cef47e7
OussamaGhz <sohaib.ghazi@outlook.fr>
2024-04-18 18:30:36 +0100

    login page

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,7 @@ export default async function Home() {
     const password = formdata.get("password") as string;
 
     console.log("Form data", email, username, password);
+    
 
     // try {
     //   const user = await prisma.user.create({
@@ -27,5 +28,28 @@ export default async function Home() {
     // }
   };
 
-  return <div></div>;
+  return (
+    <form
+      className="flex min-h-screen flex-col items-center justify-between p-24 text-black"
+      action={addUser}
+    >
+      <div className="flex flex-col items-center">
+        <h1 className="text-4xl font-bold">Welcome to Next.js</h1>
+        <p className="text-lg text-center mt-4">Get started by editing </p>
+      </div>
+      <div className="flex flex-col items-center">
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" required />
+
+        <label htmlFor="username">Username:</label>
+        <input type="text" id="username" name="username" required />
+
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" required />
+        <button type="submit" className="bg-red-200">
+          submit
+        </button>
+      </div>
+    </form>
+  );
 }
