0cdcfaec59145d39de49ad86dfe6b777394b5e13
OussamaGhz <sohaib.ghazi@outlook.fr>
2024-05-06 04:50:30 +0100

    changing moadals

--- app/dashboard/salles/page.tsx
+++ app/dashboard/salles/page.tsx
@@ -6,6 +6,18 @@ import { Button } from "@/components/ui/button";
 import PlusIcon from "@/app/ui/icon/plus-icon";
 import { PrismaClient } from "@prisma/client";
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 const prisma = new PrismaClient();
 
 const EnseignantsPage = async () => {
@@ -39,13 +51,59 @@ const EnseignantsPage = async () => {
             <h1 className="font-[600] text-[40px] text-left my-[30px] ">
               Salles
             </h1>
-            <Button
-              variant="default"
-              className="flex gap-2 px-[15px] py-[22px] max:w-56  text-white bg-[#4A58EC] rounded-[11px]"
-            >
-              <PlusIcon />
-              <p>Ajouter nouvelle salle</p>
-            </Button>
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button
+                  variant="default"
+                  className="flex gap-2 px-[15px] py-[22px] max:w-56  text-white bg-[#4A58EC] rounded-[11px]"
+                >
+                  <PlusIcon />
+                  Ajouter une salle
+                </Button>
+              </DialogTrigger>
+              <DialogContent className="sm:max-w-[800px] bg-white">
+                <DialogHeader>
+                  <DialogTitle className="text-[28px] text-[#001D74]">
+                    Ajouter une salle
+                  </DialogTitle>
+                </DialogHeader>
+                <div className="py-[20px] text-black flex flex-col gap-6 text-left border-b-[1px] border-gray-200">
+                  <div className="flex justify-between items-center gap-8">
+                    <div className="flex flex-col w-[50%] text-left">
+                      <Label className="text-[20.051px] font-[400] my-3">
+                        Nom de la salle
+                      </Label>
+                      <Input className="w-full h-[52px]" />
+                    </div>
+                    <div className="flex flex-col w-[50%]">
+                      <Label className="text-[20.051px] font-[400] my-3">
+                        Type de la salle
+                      </Label>
+                      <Input className="w-full h-[52px]" />
+                    </div>
+                  </div>
+                  <div className="flex justify-between items-center gap-8">
+                    <div className="flex flex-col w-[50%] text-left">
+                      <Label className="text-[20.051px] font-[400] my-3">
+                        Capacite
+                      </Label>
+                      <Input className="w-full h-[52px]" />
+                    </div>
+                    <div className="flex flex-col w-[50%]">
+                      <Label className="text-[20.051px] font-[400] my-3">
+                        Disponibilite
+                      </Label>
+                      <Input className="w-full h-[52px]" />
+                    </div>
+                  </div>
+                </div>
+                <DialogFooter>
+                  <Button type="submit" className="text-white">
+                    Sauvegarder
+                  </Button>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
           </div>
 
           <DataTable columns={columns} data={data} />
--- app/ui/salles/columns.tsx
+++ app/ui/salles/columns.tsx
@@ -21,6 +21,18 @@ import Loading from "../icon/loading";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import DeleteIcon from "../icon/delete-icon";
+
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
 export type Room = {
@@ -88,6 +100,13 @@ export const columns: ColumnDef<Room>[] = [
       const [showUpdateModal, setShowUpdateModal] = useState(false);
       const [isDeleting, setIsDeleting] = useState<boolean>(false);
 
+      const [isEditDialogOpen, setIsEditDialogOpen] = useState(false);
+      const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false);
+
+      const updateHandler = async () => {
+        alert("update handler");
+      };
+
       const handleDelete = async () => {
         // delete action use fetch api route
         try {
@@ -118,6 +137,107 @@ export const columns: ColumnDef<Room>[] = [
 
       return (
         <>
+          <Dialog
+            open={isEditDialogOpen}
+            onOpenChange={(open) => setIsEditDialogOpen(open)}
+          >
+            <DialogContent className="sm:max-w-[800px] bg-white">
+              <DialogHeader>
+                <DialogTitle className="text-[28px] text-[#001D74]">
+                  Modifier Salle
+                </DialogTitle>
+              </DialogHeader>
+              <div className="py-[20px] text-black flex flex-col gap-6 text-left border-b-[1px] border-gray-200">
+                <div className="flex justify-between items-center gap-8">
+                  <div className="flex flex-col w-[50%] text-left">
+                    <Label className="text-[20.051px] font-[400] my-3">
+                      Nom de la salle
+                    </Label>
+                    <Input
+                      className="w-full h-[52px]"
+                      placeholder={payment.nom_salle}
+                    />
+                  </div>
+                  <div className="flex flex-col w-[50%]">
+                    <Label className="text-[20.051px] font-[400] my-3">
+                      Type de la salle
+                    </Label>
+                    <Input
+                      className="w-full h-[52px]"
+                      placeholder={payment.type_salle}
+                    />
+                  </div>
+                </div>
+                <div className="flex justify-between items-center gap-8">
+                  <div className="flex flex-col w-[50%] text-left">
+                    <Label className="text-[20.051px] font-[400] my-3">
+                      Capacite
+                    </Label>
+                    <Input
+                      className="w-full h-[52px]"
+                      placeholder={payment.capacity}
+                    />
+                  </div>
+                  <div className="flex flex-col w-[50%]">
+                    <Label className="text-[20.051px] font-[400] my-3">
+                      Disponibilite
+                    </Label>
+                    <Input
+                      placeholder={payment.disponibilite.join(",")}
+                      className="w-full h-[52px]"
+                    />
+                  </div>
+                </div>
+              </div>
+              <DialogFooter>
+                <Button type="submit" className="text-white ">
+                  Sauvegarder
+                </Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+
+          <Dialog
+            open={isDeleteDialogOpen}
+            onOpenChange={(open) => setIsDeleteDialogOpen(open)}
+          >
+            <DialogContent className="sm:max-w-[425px]">
+              <div className="flex justify-center items-center my-5 flex-col gap-5">
+                <svg
+                  width="85"
+                  height="85"
+                  viewBox="0 0 85 85"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M42.5003 0.416992C65.7429 0.416992 84.5836 19.2577 84.5836 42.5003C84.5836 65.7429 65.7429 84.5836 42.5003 84.5836C19.2577 84.5836 0.416992 65.7429 0.416992 42.5003C0.416992 19.2577 19.2577 0.416992 42.5003 0.416992ZM42.5003 8.83366C33.5714 8.83366 25.0081 12.3807 18.6944 18.6944C12.3807 25.0081 8.83366 33.5714 8.83366 42.5003C8.83366 51.4293 12.3807 59.9925 18.6944 66.3063C25.0081 72.62 33.5714 76.167 42.5003 76.167C51.4293 76.167 59.9925 72.62 66.3063 66.3063C72.62 59.9925 76.167 51.4293 76.167 42.5003C76.167 33.5714 72.62 25.0081 66.3063 18.6944C59.9925 12.3807 51.4293 8.83366 42.5003 8.83366ZM42.5003 55.1253C43.6164 55.1253 44.6868 55.5687 45.4761 56.3579C46.2653 57.1471 46.7087 58.2175 46.7087 59.3337C46.7087 60.4498 46.2653 61.5202 45.4761 62.3094C44.6868 63.0986 43.6164 63.542 42.5003 63.542C41.3842 63.542 40.3138 63.0986 39.5246 62.3094C38.7354 61.5202 38.292 60.4498 38.292 59.3337C38.292 58.2175 38.7354 57.1471 39.5246 56.3579C40.3138 55.5687 41.3842 55.1253 42.5003 55.1253ZM42.5003 17.2503C43.6164 17.2503 44.6868 17.6937 45.4761 18.4829C46.2653 19.2721 46.7087 20.3425 46.7087 21.4587V46.7087C46.7087 47.8248 46.2653 48.8952 45.4761 49.6844C44.6868 50.4736 43.6164 50.917 42.5003 50.917C41.3842 50.917 40.3138 50.4736 39.5246 49.6844C38.7354 48.8952 38.292 47.8248 38.292 46.7087V21.4587C38.292 20.3425 38.7354 19.2721 39.5246 18.4829C40.3138 17.6937 41.3842 17.2503 42.5003 17.2503Z"
+                    fill="#E02424"
+                  />
+                </svg>
+                <p className="text-[#556476] text-[24px] text-center">
+                  Etes vous sure de vouloire supprimer cette salle?
+                </p>
+              </div>
+
+              <div className="flex justify-center items-center gap-5">
+                <Button
+                  variant="ghost"
+                  onClick={() => setIsDeleteDialogOpen(false)}
+                  className="text-[16px]"
+                >
+                  Non, annuler
+                </Button>
+                <Button
+                  variant="destructive"
+                  className="text-[16px]"
+                  onClick={handleDelete}
+                >
+                  Oui, je suis sure
+                </Button>
+              </div>
+            </DialogContent>
+          </Dialog>
           <DropdownMenu>
             <DropdownMenuTrigger asChild ref={triggerRef}>
               <Button variant="ghost" className="h-8 w-8 p-0">
@@ -130,160 +250,20 @@ export const columns: ColumnDef<Room>[] = [
               <DropdownMenuSeparator />
               <DropdownMenuItem className="flex gap-2 items-center  bg-[#5B93FF] bg-opacity-5 py-1 px-4 my-1 text-[#5B93FF] ">
                 <Edit3 className="h-4 w-4" />
-                <button onClick={() => setShowUpdateModal(true)}>
-                  Modifier la salle
-                </button>
+                <DropdownMenuItem onClick={() => setIsEditDialogOpen(true)}>
+                  Edit
+                </DropdownMenuItem>
               </DropdownMenuItem>
+              {/* delete dialog */}
+
               <DropdownMenuItem className="flex gap-2 items-center bg-[#E71D36] text-[#E71D36] bg-opacity-5 py-1 px-4 my-1">
                 <Trash className="h-4 w-4" />
-                <button onClick={() => setShowConfirmationModal(true)}>
-                  Supprimer la salle
-                </button>
+                <DropdownMenuItem onClick={() => setIsDeleteDialogOpen(true)}>
+                  Delete
+                </DropdownMenuItem>
               </DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
-
-          {showConfirmationModal && (
-            <div className="fixed inset-0 flex items-center justify-center z-50  rounded-lg shadow">
-              <div className="bg-white rounded-lg animate-fade-in">
-                <div className="p-4 md:p-5 text-cente m-4">
-                  <div className="flex justify-end">
-                    <MdClose
-                      className="h-6 w-6 cursor-pointer"
-                      onClick={handleToggleModal}
-                    />
-                  </div>
-                  <div className="flex justify-center items-center my-7">
-                    <svg
-                      width="85"
-                      height="85"
-                      viewBox="0 0 85 85"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <g id="Warn">
-                        <g id="Group">
-                          <path
-                            id="Vector"
-                            d="M42.5003 0.416992C65.7429 0.416992 84.5836 19.2577 84.5836 42.5003C84.5836 65.7429 65.7429 84.5836 42.5003 84.5836C19.2577 84.5836 0.416992 65.7429 0.416992 42.5003C0.416992 19.2577 19.2577 0.416992 42.5003 0.416992ZM42.5003 8.83366C33.5714 8.83366 25.0081 12.3807 18.6944 18.6944C12.3807 25.0081 8.83366 33.5714 8.83366 42.5003C8.83366 51.4293 12.3807 59.9925 18.6944 66.3063C25.0081 72.62 33.5714 76.167 42.5003 76.167C51.4293 76.167 59.9925 72.62 66.3063 66.3063C72.62 59.9925 76.167 51.4293 76.167 42.5003C76.167 33.5714 72.62 25.0081 66.3063 18.6944C59.9925 12.3807 51.4293 8.83366 42.5003 8.83366ZM42.5003 55.1253C43.6164 55.1253 44.6868 55.5687 45.4761 56.3579C46.2653 57.1471 46.7087 58.2175 46.7087 59.3337C46.7087 60.4498 46.2653 61.5202 45.4761 62.3094C44.6868 63.0986 43.6164 63.542 42.5003 63.542C41.3842 63.542 40.3138 63.0986 39.5246 62.3094C38.7354 61.5202 38.292 60.4498 38.292 59.3337C38.292 58.2175 38.7354 57.1471 39.5246 56.3579C40.3138 55.5687 41.3842 55.1253 42.5003 55.1253ZM42.5003 17.2503C43.6164 17.2503 44.6868 17.6937 45.4761 18.4829C46.2653 19.2721 46.7087 20.3425 46.7087 21.4587V46.7087C46.7087 47.8248 46.2653 48.8952 45.4761 49.6844C44.6868 50.4736 43.6164 50.917 42.5003 50.917C41.3842 50.917 40.3138 50.4736 39.5246 49.6844C38.7354 48.8952 38.292 47.8248 38.292 46.7087V21.4587C38.292 20.3425 38.7354 19.2721 39.5246 18.4829C40.3138 17.6937 41.3842 17.2503 42.5003 17.2503Z"
-                            fill="#E02424"
-                          />
-                        </g>
-                      </g>
-                    </svg>
-                  </div>
-
-                  <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-                    Etes vous sure de vouloire supprimer cette salle?
-                  </h3>
-                  <div className="flex gap-4 justify-center">
-                    <Button
-                      variant={"destructive"}
-                      className="rounded-lg"
-                      onClick={handleDelete}
-                      disabled={isDeleting}
-                    >
-                      {isDeleting ? <Loading /> : "Oui, je suis sure"}
-                    </Button>
-                    <Button
-                      onClick={handleToggleModal}
-                      variant={"outline"}
-                      className="rounded-lg"
-                    >
-                      Non, annuler
-                    </Button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          )}
-
-          {showUpdateModal && (
-            <div className="fixed inset-0 flex items-center justify-center z-50  rounded-lg shadow">
-              <div className="bg-white   rounded-lg animate-fade-in">
-                <div className="p-4 md:p-5 text-cente min-w-[800px] h-[461px]">
-                  <div className="flex justify-end">
-                    <MdClose
-                      className="h-6 w-6 cursor-pointer"
-                      onClick={() => setShowUpdateModal(!showUpdateModal)}
-                    />
-                  </div>
-                  <div>
-                    {/* hearder */}
-                    <div className="border-b-[1px] border-gray-200 pb-[20px]">
-                      <h3 className="text-[28px] text-[#001D74] font-[600] text-left">
-                        Modifier la salle
-                      </h3>
-                    </div>
-                    {/* form */}
-                    <div className="py-[20px] text-black flex flex-col gap-6 text-left border-b-[1px] border-gray-200">
-                      <div className="flex justify-between items-center gap-8">
-                        <div className="flex flex-col w-[50%] text-left">
-                          <Label className="text-[20.051px] font-[400] my-3">
-                            Nom de la salle
-                          </Label>
-                          <Input
-                            className="w-full h-[52px]"
-                            placeholder={payment.nom_salle}
-                          />
-                        </div>
-                        <div className="flex flex-col w-[50%]">
-                          <Label className="text-[20.051px] font-[400] my-3">
-                            Type de la salle
-                          </Label>
-                          <Input
-                            className="w-full h-[52px]"
-                            placeholder={payment.type_salle}
-                          />
-                        </div>
-                      </div>
-                      <div className="flex justify-between items-center gap-8">
-                        <div className="flex flex-col w-[50%] text-left">
-                          <Label className="text-[20.051px] font-[400] my-3">
-                            Capacite
-                          </Label>
-                          <Input
-                            className="w-full h-[52px]"
-                            placeholder={payment.capacity}
-                          />
-                        </div>
-                        <div className="flex flex-col w-[50%]">
-                          <Label className="text-[20.051px] font-[400] my-3">
-                            Disponibilite
-                          </Label>
-                          <Input
-                            placeholder={payment.disponibilite.join(",")}
-                            className="w-full h-[52px]"
-                          />
-                        </div>
-                      </div>
-                    </div>
-
-                    <div className="flex justify-center">
-                      <Button variant="destructive">Update</Button>
-                      <Button
-                        variant="outline"
-                        onClick={() => setShowUpdateModal(!showUpdateModal)}
-                      >
-                        Cancel
-                      </Button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          )}
-
-          {(showConfirmationModal || showUpdateModal) && (
-            <div
-              className={`fixed inset-0 ${
-                showConfirmationModal && "bg-red-500"
-              }   ${
-                showUpdateModal && "bg-black"
-              }  opacity-50 z-40 overflow-hidden`}
-              style={{ pointerEvents: "none" }}
-            ></div>
-          )}
         </>
       );
     },
