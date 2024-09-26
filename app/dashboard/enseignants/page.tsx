86b45c99cb4318e3633ca53d05b8ebf8776186d6
Oussama <sohaib.ghazi@outlook.fr>
2024-09-26 21:34:27 +0100

    fix build issues

--- app/dashboard/enseignants/page.tsx
+++ app/dashboard/enseignants/page.tsx
@@ -4,11 +4,11 @@ import React from "react";
 import { Teacher, columns } from "@/app/ui/enseignants/columns";
 import { DataTable } from "@/app/ui/enseignants/data-table";
 import AddUser from "@/app/ui/enseignants/add_user-modal";
-import { useenseignant } from "@/app/utils/fetchers";
+import { useEnseignant } from "@/app/utils/fetchers";
 import { useAppContext } from "@/app/store/context";
 
 const EnseignantsPage = () => {
-  const { enseignant, loading } = useenseignant();
+  const { enseignant, loading } = useEnseignant();
 
   const data: Teacher[] = enseignant.map((enseignant: Teacher) => {
     return {
